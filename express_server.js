bdf14b75284ad636bc606f89a71458042e25fd13
William <williamwijaya.mail@gmail.com>
2022-04-19 01:31:38 +0000

    EJS

--- express_server.js
+++ express_server.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+
+function generateRandomString() {
+    return Math.random().toString(36).slice(2, 8);
+};
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,12 +25,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
 // });
 
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
-  
+
   const shortURL = req.params.shortURL;
 
   const templateVars = {
